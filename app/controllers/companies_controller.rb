8a0f4c6ca3f1a0d1fa2cf261c6fd23a7b3f3538e
Zach Schneider <zach@schneid.io>
2018-10-09 15:34:32 -0400

    Add external links, use png for Leandog logo so it behaves better

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,35 +3,43 @@ class CompaniesController < ApplicationController
     @companies = [
       {
         name: "RVshare",
-        image: "images/rvshare.jpg"
+        image: "images/rvshare.jpg",
+        url: "https://rvshare.com"
       },
       {
         name: "Coffee and Code",
-        image: "images/coffee_and_code.png"
+        image: "images/coffee_and_code.png",
+        url: "https://www.coffeeandcode.com"
       },
       {
         name: "CoverMyMeds",
-        image: "images/covermymeds.png"
+        image: "images/covermymeds.png",
+        url: "https://www.covermymeds.com"
       },
       {
         name: "LevelSeven",
-        image: "images/level_seven.jpeg"
+        image: "images/level_seven.jpeg",
+        url: "https://www.lvlsvn.com"
       },
       {
         name: "Within3",
-        image: "images/within3.png"
+        image: "images/within3.png",
+        url: "https://www.within3.com"
       },
       {
         name: "University Tees",
-        image: "images/u_tees.jpg"
+        image: "images/u_tees.jpg",
+        url: "https://universitytees.com"
       },
       {
         name: "LeanDog",
-        image: "images/leandog.svg"
+        image: "images/leandog.png",
+        url: "https://www.leandog.com"
       },
       {
         name: "VHT",
-        image: "images/vht.png"
+        image: "images/vht.png",
+        url: "https://www.vhtcx.com"
       }
     ].shuffle
   end
